63d56d7145d79f49f3bf2a332487b357ce70ab4b
Michael Lahr <michael.lahr@gmail.com>
2025-08-12 11:22:34 +0800

    refactor: Simplify command imports in shell.ts

--- src/cli/shell.ts
+++ src/cli/shell.ts
@@ -1,23 +1,8 @@
-/**
- * Copyright (c) Microsoft Corporation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 import * as readline from 'readline';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as os from 'os';
-import {cdCommand, Command, dateCommand, echoCommand, lsCommand, pwdCommand} from './commands/index.js';
+import {Command} from './commands/index.js';
 
 // Command registry to store registered commands
 class CommandRegistry {
